215ba2ad91126eb7349b54c2ec5c5cbf30f781df
JOB KIPTOO <114221274+KIPTOO-JOB@users.noreply.github.com>
2024-07-20 20:42:02 +0000

    init

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+const mainElement = document.querySelector('main#main');
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+
+document.body.appendChild(newHeader);
+
+mainElement.remove();
